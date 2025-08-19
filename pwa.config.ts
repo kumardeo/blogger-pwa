9a3c28e7ae9db75373449b1371fb4ff1d9a34998
Piush Trivedi <piyush.ptri005@gmail.com>
2025-08-19 16:04:56 +0530

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,9 +2,9 @@ import type { Config } from './types';
 
 export default {
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'Asha Real Estate',
+  shortName: 'Asha Real Estate',
+  description: 'Asha Real Estate description goes here...',
   direction: 'auto',
   language: 'en-US',
   backgroundColor: '#fff',
@@ -39,5 +39,5 @@ export default {
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'https://asharealestate.asnp.in',
 } satisfies Config;
